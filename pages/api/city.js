34fec1d15397e6b6a7afa17578bd2cbb3e59e3f0
Andro Mikulić <andromikulic@gmail.com>
2021-10-11 00:10:02 +0200

    Return city name in response.

--- pages/api/city.js
+++ pages/api/city.js
@@ -2,7 +2,7 @@ const https = require('https')
 import * as config from 'config.js'
 
 export default async function handler(req, res) {
-    const { lat, lng } = req.query
+    const { city, lat, lng } = req.query
     if (!lat || !lng) {
         res.status(400).json({
             error: 'Bad lattitude or longitude',
@@ -10,10 +10,10 @@ export default async function handler(req, res) {
         return
     }
     const url = 'https://api.troposphere.io/climate/' + lat + ',' + lng + '?token=' + config.apiKey
-    await getDataFromAPI(url, res)
+    await getDataFromAPI(city, url, res)
 }
 
-async function getDataFromAPI(url, res) {
+async function getDataFromAPI(city, url, res) {
     return new Promise(resolve => {
         https.get(url, response => {
             let data = ''
@@ -23,7 +23,9 @@ async function getDataFromAPI(url, res) {
             })
 
             response.on('end', () => {
-                res.status(200).json(JSON.parse(data))
+                let json = JSON.parse(data)
+                json.city = city
+                res.status(200).json(json)
                 Promise.resolve();
             })
 
