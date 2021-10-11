e58b721041d6d4c0aa069cc1b9cc7ea5e7b6e9f2
Andro Mikulić <andromikulic@gmail.com>
2021-10-11 21:42:50 +0200

    Save cache to file.

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    apiKey: '7b20b1b908b9119e34c22a567209746514c8ad610e77d52d5b'
+    apiKey: '7b20b1b908b9119e34c22a567209746514c8ad610e77d52d5b',
+    cacheFile: 'cache.json',
+    cacheSaveInterval: 10
 }
--- pages/api/city.js
+++ pages/api/city.js
@@ -1,18 +1,20 @@
 const https = require('https')
-const csv = require("csvtojson")
+const fs = require('fs');
 import * as config from 'config.js'
 
-let database = {}
+let citiesCache = {}
+let cacheCount = 0
+let cacheLoaded = false
 
 export default async function handler(req, res) {
-
+    loadCache()
     let { id } = req.query
     let splitId = id.split('_')
     let lat = splitId[1]
     let lng = splitId[2]
 
     if (cityCached(id)) {
-        res.status(200).send(database[id])
+        res.status(200).send(citiesCache[id])
         return
     }
     const url = 'https://api.troposphere.io/climate/' + lat + ',' + lng + '?token=' + config.apiKey
@@ -23,11 +25,33 @@ function cacheCity(id, json) {
     let splitId = id.split('_')
     let name = splitId[0]
     json.data.city = name
-    database[id] = json
+    citiesCache[id] = json
+    saveCache()
+}
+
+function loadCache() {
+    if (cacheLoaded) {
+        return
+    }
+    cacheLoaded = true
+    citiesCache = JSON.parse(fs.readFileSync(config.cacheFile, 'utf8'))
+    console.log('Loaded cache:')
+    console.log(citiesCache)
+}
+
+function saveCache() {
+    cacheCount++
+    if (cacheCount === config.cacheSaveInterval) {
+        cacheCount = 0
+        fs.writeFile(config.cacheFile, JSON.stringify(citiesCache), function (err) {
+            if (err) return console.log(err)
+            console.log('Saved cache')
+        })
+    }
 }
 
 function cityCached(id) {
-    if (id in database) {
+    if (id in citiesCache) {
         return true
     }
     return false
@@ -45,7 +69,7 @@ async function getDataFromAPI(id, url, res) {
             response.on('end', () => {
                 let json = JSON.parse(data)
                 cacheCity(id, json)
-                res.status(200).json(database[id])
+                res.status(200).json(citiesCache[id])
                 Promise.resolve();
             })
 
