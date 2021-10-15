05724d1f90f9a0b411c68667158c5b3538092389
Andro Mikulić <andromikulic@gmail.com>
2021-10-15 23:05:40 +0200

    initialize log file in config.

--- config.js
+++ config.js
@@ -1,11 +1,25 @@
 const fs = require('fs')
 
-const cacheFile = fs.realpathSync('cache.json')
+let log_file = fs.createWriteStream('debug.log', { flags: 'a' })
+log('Open or create debug.log file.')
+
 const logFile = fs.realpathSync('debug.log')
+let cacheFile
+try {
+    cacheFile = fs.realpathSync('cache.json')
+} catch { }
 
 module.exports = {
     apiKey: '7b20b1b908b9119e34c22a567209746514c8ad610e77d52d5b',
     cacheFile: cacheFile,
     logFile: logFile,
     cacheSaveInterval: 8
+}
+
+function log(data) {
+    let logTime = new Date().toISOString()
+    logTime = logTime.replace('T', ' ').replace('Z', '')
+    const logEntry = logTime + ' - ' + util.format(data)
+    log_file.write(logEntry + '\n')
+    console.log(logEntry)
 }
