087c3124802421c4f10ab41e4a004ce2a299a13c
Andro Mikulić <andromikulic@gmail.com>
2021-10-15 23:52:33 +0200

    Idk.

--- pages/api/autocomplete.js
+++ pages/api/autocomplete.js
@@ -12,6 +12,8 @@ export default async function handler(req, res) {
         init = true
         try {
             cities = await csv().fromFile(config.citiesCSV)
+            res.end('Loading cities file...')
+            return
         } catch (error) {
             res.status(500).end('Error')
             return
