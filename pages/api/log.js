273db5341af28491ac50173d8bbf20f682e2016c
Andro Mikulić <andromikulic@gmail.com>
2021-10-15 22:42:50 +0200

    Handle log API errors.

--- pages/api/log.js
+++ pages/api/log.js
@@ -2,5 +2,11 @@ const fs = require('fs')
 const config = require('../../config.js')
 
 export default async function handler(req, res) {
-    res.status(200).end(fs.readFileSync(config.logFile, 'utf8'))
+    try {
+        let content = fs.readFileSync(config.logFile, 'utf8')
+        res.status(200).end(content)
+    } catch {
+        res.status(500).end('There was a problem when trying to read the logfile.')
+    }
+
 }
