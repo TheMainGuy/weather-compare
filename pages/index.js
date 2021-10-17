3eb167129435034f802add43d5a019caf7972cf6
Tin Ceraj <tin@viatu.com>
2021-10-17 21:59:43 +0200

    responsive height

--- pages/index.js
+++ pages/index.js
@@ -78,6 +78,7 @@ export default function Home() {
                 bgcolor: 'background.default',
                 color: 'text.primary',
                 p: 3,
+                overflowY: 'auto'
             }}
         >
             <div className="toggle">
