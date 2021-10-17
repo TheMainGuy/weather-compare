ae61b06e0d93c36cd5c018d10c3dd2161f7c10b1
Andro Mikulić <andromikulic@gmail.com>
2021-10-17 18:58:43 +0200

    Require secret to get the log file.

--- pages/api/log.js
+++ pages/api/log.js
@@ -5,6 +5,10 @@ let { serverRuntimeConfig } = getConfig()
 const config = serverRuntimeConfig.config
 
 export default async function handler(req, res) {
+    if (!(req.query.secret === config.secret)) {
+        res.status(404).end('Not found')
+        return
+    }
     try {
         let content = fs.readFileSync(config.logFile, 'utf8')
         res.status(200).end(content)
